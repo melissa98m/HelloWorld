204388a1fc794d407dac749b2f1b03f6037fdd38
melissa98m <melissa.mangione@gmail.com>
2021-08-03 10:00:52 +0200

    exemple interpolation

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-list.component.css']
 })
 export class CarsListComponent implements OnInit {
-
+  mardiMatin:any = "On est mardi Matin!";// exemple d'interpolation type string 
+  age:number = 13; // exemple interpolation number
+  
+  //2eme exemples objet ou propriété
+  game: any = {
+    title: "BF 2042",
+    support: "Xbox Serie X",
+    multi: 128
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  getStock(){   //3eme exemple methode interpolatiion
+    return 2050 ;
+  }
 }

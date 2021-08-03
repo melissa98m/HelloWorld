2632b75ec53007383ad446f3a14331bd59f2f330
melissa98m <melissa.mangione@gmail.com>
2021-08-03 09:22:14 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 
@@ -9,6 +10,8 @@ const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'cars' , component: CarsListComponent},  //liste des routes 
   { path: 'drivers' , component: DriversListComponent},
+  { path: 'not-found' , component: FourOhFourComponent},
+  { path: '**' , redirectTo: 'not-found' },
 ];
 
 @NgModule({

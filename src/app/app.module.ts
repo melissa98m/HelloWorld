47aea17d52afd9f12e243389409b9b9e9bbb22a7
melissa98m <melissa.mangione@gmail.com>
2021-08-09 10:33:53 +0200

    drivers item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { CarItemComponent } from './cars-list/car-item/car-item.component';
 import { FormsModule } from '@angular/forms';
+import { DriverItemComponent } from './drivers-list/driver-item/driver-item.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { FormsModule } from '@angular/forms';
     HomePageComponent,
     FourOhFourComponent,
     CarItemComponent,
+    DriverItemComponent,
     
   ],
   imports: [

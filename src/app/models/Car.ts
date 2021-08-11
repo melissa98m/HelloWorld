58090036dd00c45b729f72a370fec055e1e23621
melissa98m <melissa.mangione@gmail.com>
2021-08-11 14:36:54 +0200

    creation class car

---
+++ src/app/models/Car.ts
@@ -0,0 +1,24 @@
+
+
+/**
+ * Class qui represente la structure de cars
+ * 
+ */
+export class Car{
+
+    public name:string;
+    public  pays:string;
+    public coverImage:string;
+    public power:number;
+    public perf:number;
+
+    constructor(name:string, pays:string, coverImage:string, power:number, perf:number) {
+        this.name = name;
+        this.pays = pays;
+        this.coverImage = coverImage;
+        this.power = power;
+        this.perf = perf;
+
+    }
+}
+

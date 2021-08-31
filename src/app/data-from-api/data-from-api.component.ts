29e76e057ff02222a6a5e21f2eba8c55f35212f7
melissa98m <melissa.mangione@gmail.com>
2021-08-31 16:51:40 +0200

    relier l'api au projt

--- src/app/data-from-api/data-from-api.component.ts
+++ src/app/data-from-api/data-from-api.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataFromApiComponent implements OnInit {
 
+  test:any;
   constructor(private http:HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { // recupere l'api de symfony en local
+    this.http.get<any>("localhost:8000/api").subscribe( (data: any)=> {this.test = data})
   }
 
 }
